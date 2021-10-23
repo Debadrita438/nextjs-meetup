fad0d7f0e4a7e92f95cff5b54596b9168ecb276d
Debadrita Bandyopadhyay <bristibandyopadhyay@gmail.com>
2021-10-23 15:57:39 +0530

    Applying meetup details page

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../src/components/Meetups/MeetupDetail';
+
+const MeetupDetails = () => {
+    return (
+        <MeetupDetail
+            image='https://cdn.vox-cdn.com/thumbor/Ksi3v8e1tj1ZcQeRV1ZtmCYIM7k=/0x0:4284x2916/1200x800/filters:focal(1800x1116:2484x1800)/cdn.vox-cdn.com/uploads/chorus_image/image/61918529/NorthBroadSt_Landscape_1_M.Edlow.0.jpg'
+            title='A First Meetup'
+            description='This is a first meetup'
+            address='Some Address 12, Some city, 123456'
+        />
+    );
+};
+
+export default MeetupDetails;
